393566031fa05bef6656838412b58b62774b24bd
unknown <dylanosborn123@hotmail.com>
2023-10-14 19:36:00 -0500

    fixed localStorage in Javascript

--- assets/script.js
+++ assets/script.js
@@ -2,43 +2,54 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var todoItemGet = $('#todoItemGet')
-var todoItemSet = $('#todoItemSet')
+// var todoItemGet = $('#todoItemGet')
+// var todoItemSet = $('#todoItemSet')
+// const todoItemGetJSON = JSON.stringify(todoItemGet);
+
+// $(function saveTodoItem() {
+//   $("button.saveBtn").on("click", function() {
+//     localStorage.setItem("todo", todoItemGetJSON);
+//   });
+// });
+
+
+// $(function displayTodoItem() {
+//   localStorage.getItem("todo");
+// });
+
+$(function () {
+  var todoItemsSet = $(".todoItemsSet");
+  var todoItemsGet = $(".todoItemsGet");
+  
+  var savedTodo = localStorage.getItem("todo");
+  if (savedTodo) {
+    todoItemsSet.val(savedTodo);
+  }
+
+  $("button.saveBtn").on("click", function () {
+    var todoItemsSetVal = todoItemsSet.val();
+    localStorage.setItem("todo", todoItemsSetVal);
+    $(todoItemsGet).text(todoItemsSetVal);
+  });
+
+});
 
 
 
 
 
 
-$(function saveTodoItem() {
+
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
-  $("button.saveBtn").on("click", function() {
-    localStorage.setItem("todo", todoItem);
-
-  });
-
-});
-
-
-$(function displayTodoItem() {
-  localStorage.getItem("todo");
-});
-
-
-
-
-
-
-
-
-
-
 
 
 
